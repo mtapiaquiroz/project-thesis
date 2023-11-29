2479c30128a7d6756bd4247ce93d6b2eb469510c
Maximiliano Tapia <m.tapiaquiroz@gmail.com>
2023-11-29 00:15:09 -0300

    added logs of local execution

--- apache-spark/docker/run_spark_docker.sh
+++ apache-spark/docker/run_spark_docker.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-docker run --rm -it \
-  -v /home/malkavian/Documentos/Thesis/code/project-thesis/apache-spark/target/apache-spark-1.0.0-SNAPSHOT-jar-with-dependencies.jar:/resources/spark.jar \
-  -v /home/malkavian/Respaldo/thesis/data/us_accidents.csv:/resources/us_accidents.csv \
-  -v /home/malkavian/Documentos/Thesis/code/project-thesis/apache-spark/src/main/resources/us_accidents.avsc:/resources/schema.avsc \
-  --entrypoint /opt/spark/bin/spark-submit \
-  apache/spark:3.3.3-scala2.12-java11-python3-r-ubuntu \
-  --class org.thesis.AppSpark \
-  --packages org.apache.spark:spark-avro_2.12:3.3.2 \
-  --conf spark.driver.extraJavaOptions="-Divy.cache.dir=/tmp -Divy.home=/tmp" \
-  --master local[*] \
-  /resources/spark.jar \
-  /resources/us_accidents.csv \
-  /resources/schema.avsc \
-  /opt/spark/work-dir/output/
+for i in {1..10}
+do
+  echo "Execution $i:"
+  docker run --rm -it \
+    -v /home/malkavian/Documentos/Thesis/code/project-thesis/apache-spark/target/apache-spark-1.0.0-SNAPSHOT-jar-with-dependencies.jar:/resources/spark.jar \
+    -v /home/malkavian/Respaldo/thesis/data/us_accidents.csv:/resources/us_accidents.csv \
+    -v /home/malkavian/Documentos/Thesis/code/project-thesis/apache-spark/src/main/resources/us_accidents.avsc:/resources/schema.avsc \
+    --entrypoint /opt/spark/bin/spark-submit \
+    apache/spark:3.3.3-scala2.12-java11-python3-r-ubuntu \
+    --class org.thesis.AppSpark \
+    --packages org.apache.spark:spark-avro_2.12:3.3.2 \
+    --conf spark.driver.extraJavaOptions="-Divy.cache.dir=/tmp -Divy.home=/tmp" \
+    --master local[*] \
+    /resources/spark.jar \
+    /resources/us_accidents.csv \
+    /resources/schema.avsc \
+    /opt/spark/work-dir/output/
+done
