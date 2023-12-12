4c82faa11082fd38fb83530f27ba94e65132dbc8
Maximiliano Tapia <m.tapiaquiroz@gmail.com>
2023-12-11 21:47:43 -0300

    updated bash routines

--- apache-spark/experimentations/run_spark_dataproc.sh
+++ apache-spark/experimentations/run_spark_dataproc.sh
@@ -3,7 +3,13 @@
 for i in {1..10}
 do
   echo "Execution $i:"
-  gcloud dataproc jobs submit spark --cluster thesis-spark-cluster-demo --region us-central1 --properties spark.jars.packages='org.apache.spark:spark-avro_2.12:3.3.2' --class org.thesis.AppSpark \
+  gcloud dataproc jobs submit spark \
+    --cluster thesis-spark-cluster-demo \
+    --region us-central1 \
+    --properties spark.jars.packages='org.apache.spark:spark-avro_2.12:3.3.2' \
+    --class org.thesis.AppSpark \
     --jars gs://project-thesis-max/jars/apache-spark-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
-    -- gs://project-thesis-max/input/us_accidents.csv gs://project-thesis-max/input/us_accidents.avsc gs://project-thesis-max/output/apache-spark/70k/
+    -- gs://project-thesis-max/input/us_accidents.csv \
+       gs://project-thesis-max/input/us_accidents.avsc \
+       gs://project-thesis-max/output/apache-spark/7m/$i
 done
--- apache-spark/experimentations/run_spark_docker.sh
+++ apache-spark/experimentations/run_spark_docker.sh
@@ -5,7 +5,7 @@ do
   echo "Execution $i:"
   docker run --rm -it \
     -v /home/malkavian/Documentos/Thesis/code/project-thesis/apache-spark/target/apache-spark-1.0.0-SNAPSHOT-jar-with-dependencies.jar:/resources/spark.jar \
-    -v /home/malkavian/Respaldo/thesis/data/us_accidents.csv:/resources/us_accidents.csv \
+    -v /home/malkavian/Respaldo/thesis/data/us_accidents_7k.csv:/resources/us_accidents.csv \
     -v /home/malkavian/Documentos/Thesis/code/project-thesis/apache-spark/src/main/resources/us_accidents.avsc:/resources/schema.avsc \
     --entrypoint /opt/spark/bin/spark-submit \
     apache/spark:3.3.3-scala2.12-java11-python3-r-ubuntu \
